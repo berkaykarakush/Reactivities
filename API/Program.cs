acc5cc06ee4afc5c21e40c776509ed68956e18a4
berkaykarakush <berkaykaraks@outlook.com>
2023-12-15 18:47:37 +0300

    Added - DbContext, Base and Activities Controllers, SeedData

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+    // Retrieve the connection string from the configuration to set up the database connection.
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    // opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking); // Set tracking behavior
+    // opt.EnableDetailedErrors(); // Enable detailed error messages
+    // opt.UseLazyLoadingProxies(); // Enable lazy loading proxies
+});
 
 var app = builder.Build();
 
@@ -19,5 +30,19 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");    
+}
 
 app.Run();
