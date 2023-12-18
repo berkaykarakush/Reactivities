b66fb4f988bd025272891d2c01a73ea5150b5475
berkaykarakush <berkaykaraks@outlook.com>
2023-12-18 16:02:46 +0300

    Added - AutoMapper, MediatR, CQRS, Extension Method  and, Activity CRUD

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,4 +1,8 @@
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -7,21 +11,34 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
         [HttpGet] // api/activities
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query{});
         } 
         [HttpGet("{id}")] // api/activities/{id}
         public async Task<ActionResult<Activity>> GetActicity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return await Mediator.Send(new Details.Query{Id = id});
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity([FromBody]Activity activity)
+        {
+            await Mediator.Send(new Create.Command{Activity = activity});
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, [FromBody]Activity activity)
+        {
+            activity.Id = id;
+            await Mediator.Send(new Edit.Command{Activity = activity});
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await Mediator.Send(new Delete.Command{Id = id});
+            return Ok();
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -6,23 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    // Retrieve the connection string from the configuration to set up the database connection.
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-    // opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking); // Set tracking behavior
-    // opt.EnableDetailedErrors(); // Enable detailed error messages
-    // opt.UseLazyLoadingProxies(); // Enable lazy loading proxies
-});
-// add CORS policiy
-builder.Services.AddCors( opt => {
-    opt.AddPolicy("CorsPolicy",policy =>{
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
